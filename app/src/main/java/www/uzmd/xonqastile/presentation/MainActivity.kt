45eab68511d5ba42cc235388a289788e2d5b30d4
kaminashu <ertertfd89werwe@mail.ru>
2023-08-31 23:33:30 +0500

    xonqa stile master

---
+++ app/src/main/java/www/uzmd/xonqastile/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package www.uzmd.xonqastile.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import www.uzmd.xonqastile.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
